dd6eba745cc2cf5d2f655f49166a7a8b47a631e2
Qamrul-Hassan <sajal700@gmail.com>
2025-04-29 03:43:12 +0600

    home-world-sports

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    // Mock `fs` and `worker_threads` modules for the client-side
-    if (!isServer) {
-      config.node = {
-        fs: 'empty', // Mock the 'fs' module
-        worker_threads: 'empty', // Mock the 'worker_threads' module
-      };
-    }
+  webpack(config) {
     return config;
   },
 };
